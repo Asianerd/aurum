9fda0e538d758e9120535f0f01bc1749d12abfe7
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2023-09-17 16:11:59 +0800

    cache commit

--- target/debug/deps/aurum_backend-08e7a7fad59ae0e2.d
+++ target/debug/deps/aurum_backend-08e7a7fad59ae0e2.d
@@ -1,6 +1,5 @@
-D:\GithubRepos\aurum\target\debug\deps\aurum_backend-08e7a7fad59ae0e2.rmeta: src\main.rs src\post.rs
+D:\GithubRepos\aurum\target\debug\deps\aurum_backend-08e7a7fad59ae0e2.rmeta: src\main.rs
 
-D:\GithubRepos\aurum\target\debug\deps\aurum_backend-08e7a7fad59ae0e2.d: src\main.rs src\post.rs
+D:\GithubRepos\aurum\target\debug\deps\aurum_backend-08e7a7fad59ae0e2.d: src\main.rs
 
 src\main.rs:
-src\post.rs:
--- target/debug/deps/aurum_backend.d
+++ target/debug/deps/aurum_backend.d
@@ -1,6 +1,7 @@
-d:\GithubRepos\aurum\target\debug\deps\aurum_backend.exe: src\main.rs src\post.rs
+d:\GithubRepos\aurum\target\debug\deps\aurum_backend.exe: src\main.rs src\post.rs src\user.rs
 
-d:\GithubRepos\aurum\target\debug\deps\aurum_backend.d: src\main.rs src\post.rs
+d:\GithubRepos\aurum\target\debug\deps\aurum_backend.d: src\main.rs src\post.rs src\user.rs
 
 src\main.rs:
 src\post.rs:
+src\user.rs:
